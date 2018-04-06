9397d04a8a6e7a44948cad916593f3e2dcccc535
Du Park <dupark3@gmail.com>
2018-04-06 12:50:45 -0400

    Exercise 15-1 testing outlined. No working version yet

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std;
+#include "Picture.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cout << "Enter your first string picture: " << std::endl;
+    std::vector<std::string> input;
+    std::string s;
+    while(std::cin >> s){
+        input.push_back(s);
+    }
+
+    Picture p = input;
+    Picture q = frame(p);
+    Picture r = hcat(p, q);
+    Picture s = vcat(q, r);
+    std::cout << frame(hcat(s, vcat(r, q))) << std::endl;
+
     return 0;
 }
